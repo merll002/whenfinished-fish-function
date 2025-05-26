2e8dc55619998fbf974ab0860788737598e75e24
Slapdoge <76182388+merll002@users.noreply.github.com>
2025-05-26 13:34:01 +0100

    Add files via upload

---
+++ whenfinished.fish
@@ -0,0 +1,8 @@
+function whenfinished
+    if test (count $argv) -lt 2
+        echo "Usage: whenfinished <process name> [command to run when finished]"
+        return 1
+    end
+
+    while true; if pgrep $argv[1] >/dev/null; else; $argv[2..-1]; break; end; sleep 0.1; end
+end
