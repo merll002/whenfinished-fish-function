207584f463b4cf1052caea45879d09e068b9cb80
Slapdoge <76182388+merll002@users.noreply.github.com>
2025-05-26 13:46:14 +0100

    Update whenfinished.fish

--- whenfinished.fish
+++ whenfinished.fish
@@ -4,5 +4,5 @@ function whenfinished
         return 1
     end
 
-    while true; if pgrep $argv[1] >/dev/null; else; $argv[2..-1]; break; end; sleep 0.1; end
+    while true; if pgrep $argv[1] >/dev/null; else; $argv[2..-1]; break; end; sleep 0.2; end
 end
